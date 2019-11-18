3aea561b6dd8bb0d1b6848a99248ce6b1734b3fd
Manuel-Ag <46526610+Manuel-Ag@users.noreply.github.com>
2019-11-18 18:01:59 +0100

    Modificado commit ejercicio06

--- PSP2/src/ejemplo06/Hilo.java
+++ PSP2/src/ejemplo06/Hilo.java
@@ -1,10 +1,28 @@
 package ejemplo06;
 
 public class Hilo extends Thread {
-
-	public void run() {
-		int numero = 0;
-		
+	private boolean parar = false;
+	
+	public Hilo(ThreadGroup threadGroup, Thread thread, String nombre) {
+		super(threadGroup, thread, nombre);
 	}
+	
+	public void run() {
+		// System.out.println("getName(): " + Thread.currentThread().toString());
+		while (!parar) {
+				try {
+					sleep(10);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		
+		System.out.println("Hilo " + getName() + "finalizado");
 
+	}
+	
+	public void parar() {
+		this.parar = true;
+	}
 }
--- PSP2/src/ejemplo06/Main.java
+++ PSP2/src/ejemplo06/Main.java
@@ -1,39 +1,45 @@
 package ejemplo06;
 
+import java.util.Arrays;
+
 public class Main {
 
-	/**
-	 * Gestión de hilos
-	 * 
-	 * @param args
-	 * @throws InterruptedException
-	 */
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ThreadGroup threadGroup = new ThreadGroup("Grupo1");
 
-		Hilo hilo = new Hilo();
-
-		System.out.println(hilo.getState());
-
-		hilo.start();
-		Thread.sleep(1000);
-		hilo.suspend();
-
-
-
-		// while (hilo.getState() != Thread.State.TERMINATED)
-		// System.out.println(hilo.getState());
-
-		for (int i = 0; i < 200; i++)
-		    System.out.println(hilo.getState());
-
-		// Thread.sleep(1000);
-		// hilo.notify();
-		// hilo.wait();
-
-		// Thread.sleep(1000);
-		// hilo.resume();
-
+		
+		//Creo los hilos
+		Hilo thread = new Hilo(threadGroup, null, "Hilo 1");
+		Hilo thread2 = new Hilo(threadGroup, null, "Hilo 2");
+		Hilo thread3 = new Hilo(threadGroup, null, "Hilo 3");
+		
+		thread.start();
+		thread2.start();
+		thread3.start();
+		
+		
+		for (int i = 0; i < 5; i++) {
+			// list() muestra alguna informaciÃ³n del grupo
+			threadGroup.list();
+			// activeCount nos devuelve la cantidad de hilos activos
+			Thread[] estado = new Thread[threadGroup.activeCount()];
+			// enumerate() guarda en el array los hilos activos
+			threadGroup.enumerate(estado);
+			System.out.println(Arrays.toString(estado));
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		thread.parar();
+		thread2.parar();
+		thread3.parar();
+		
+		
 	}
 
 }

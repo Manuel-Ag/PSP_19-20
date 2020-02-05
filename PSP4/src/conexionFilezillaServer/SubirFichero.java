05bfef445b86e230857f5c6316a133622c7b0a26
Manuel-Ag <46526610+Manuel-Ag@users.noreply.github.com>
2020-02-05 02:51:50 +0100

    Añadido PSP4

---
+++ PSP4/src/conexionFilezillaServer/SubirFichero.java
@@ -0,0 +1,65 @@
+package conexionFilezillaServer;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class SubirFichero {
+
+	public static void main(String[] args) throws IOException {
+		String servidor = "localhost";
+		String user = "psp";
+		String pass = "psp";
+		String directorio = "/prueba";
+		String fichero = "sunny.zip";
+
+		FTPClient cliente = new FTPClient();
+		cliente.connect(servidor);
+		cliente.enterLocalPassiveMode();
+		boolean login = cliente.login(user, pass);
+
+		if (login) {
+			System.out.println("Conexión correcta");
+			// Si no existe el directorio devuelve false, en ese caso creamos el directorio
+			if (!cliente.changeWorkingDirectory(directorio)) {
+				cliente.makeDirectory(directorio);
+				System.out.println("Carpeta creada " + directorio);
+				cliente.changeWorkingDirectory(directorio);
+			}
+
+			while (true) {
+				System.out.println("1.Subir\n2.Borrar\n3.Renombrar");
+				Scanner scanner = new Scanner(System.in);
+				String key = scanner.nextLine();
+				switch (key) {
+				case "1":
+					cliente.setFileType(FTP.BINARY_FILE_TYPE);
+					BufferedInputStream in = new BufferedInputStream(new FileInputStream(fichero));
+					if (cliente.storeFile(fichero, in))
+						System.out.println("Fichero subido");
+					else
+						System.out.println("Fallo al subir");
+					break;
+				case "2":
+					
+					
+					break;
+					
+				default:
+					break;
+				}
+
+			}
+
+		} else
+			System.out.println("Incorrecto");
+
+		cliente.logout();
+		cliente.disconnect();
+	}
+
+}
---
+++ PSP4/src/ftpAnonimo/ClienteFTP.java
@@ -0,0 +1,66 @@
+package ftpAnonimo;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Scanner;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+public class ClienteFTP {
+	
+	// Tipo 0, 1 y 2 del tipo de fichero Ftp
+	private static String tipos[] = {"Fichero", "Directorio", "Enlace simb."};
+
+	public static void main(String[] args) {
+		try {
+			conexion1();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static void conexion1() throws SocketException, IOException {
+		FTPClient cliente = new FTPClient();
+		String server = "ftp.rediris.es";
+		
+		cliente.connect(server);
+		cliente.enterLocalPassiveMode();
+		
+		boolean login = cliente.login("adsf", "asdfadf");
+		int reply = cliente.getReplyCode();
+		
+		System.out.println(cliente.getReplyString() + " " + reply);
+		
+		if (!FTPReply.isPositiveCompletion(reply)) {
+			cliente.disconnect();
+			System.out.println("Error");
+		}
+		else {
+			while (true) {
+				Scanner scanner = new Scanner(System.in);
+				String cadena = scanner.nextLine();
+				if (cliente.changeWorkingDirectory(cadena)) 
+					System.out.println("Directorio actual: " + cliente.printWorkingDirectory());
+				else
+					System.out.println("Error al cambiar a " + cadena);
+				if (cadena.equals("listar")) {
+					FTPFile[] files = cliente.listFiles();
+					
+					for (int i = 0; i < files.length; i++) {
+						System.out.println("Nombre: " + files[i].getName() + " tipo: " + tipos[files[i].getType()]);
+					}
+				}
+			}
+		}	
+		cliente.disconnect();
+		
+	}
+
+}
